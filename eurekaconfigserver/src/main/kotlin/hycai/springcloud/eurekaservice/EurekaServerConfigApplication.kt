f2d269a870786fc8c69ae75ae4b3c444bb7388d2
java-hycai <1425175075@qq.com>
2018-11-09 21:25:13 +0800

    commit all

--- eurekaconfigserver/src/main/kotlin/hycai/springcloud/eurekaservice/EurekaServerConfigApplication.kt
+++ eurekaconfigserver/src/main/kotlin/hycai/springcloud/eurekaservice/EurekaServerConfigApplication.kt
@@ -2,10 +2,12 @@ package hycai.springcloud.eurekaservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 class EurekaServerConfigApplication
 
 fun main(args: Array<String>) {
--- eurekaservice/src/main/kotlin/hycai/springcloud/eurekaservice/controller/TestController.kt
+++ eurekaservice/src/main/kotlin/hycai/springcloud/eurekaservice/controller/TestController.kt
@@ -1,5 +1,6 @@
 package hycai.springcloud.eurekaservice.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -8,8 +9,10 @@ import java.util.*
 @RestController
 class TestController {
 
+
     @RequestMapping("/hello")
-   fun hello():String{
-        return "hellosdfsdf"+ Date().time
+    fun hello(): String {
+        return "hellosdfsdf" + Date().time
     }
+
 }
